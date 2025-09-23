5ea2f8af2e42bb66f8aac5a29e357cb5f43521e9
Meet <tailorhetal622@gmail.com>
2025-09-23 20:36:58 +0530

    initial commit

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+// import { ENV } from "./env.js"; ❌ Not needed anymore
+import * as schema from "../db/Schema.js";
+
+// ✅ Use only ONE sql declaration
+const sql = neon("postgresql://neondb_owner:npg_d3Ov5ftxArHJ@ep-orange-king-a9n9k94l-pooler.gwc.azure.neon.tech/neondb?sslmode=require");
+
+export const db = drizzle(sql, { schema });
